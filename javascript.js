53e7feaa5873b764bfbd313c7e51a35c11ec3273
okmanl <ericgumba@gmail.com>
2017-07-21 01:12:20 -0700

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,59 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var sudokuArr = [];
+
+function table(){
+    let strHTML = "";    
+    strHTML = "<table>";
+    for(let row = 0; row < 9; row++){
+    strHTML += "<tr>";
+        for (let col = 0; col < 9; col++){
+            strHTML += `<td><input id="r${row}c${col}" type="number" min="0" max="9" /></td>`;
+        }
+    strHTML += "</tr>";
+    }
+    strHTML += "</table>";
+    document.getElementById('myTable').innerHTML = strHTML;
+    
+    solvedTable();
+    
+};
+
+function solvedTable(){ 
+    let strHTML = "";    
+    strHTML = "<table>";
+    for(let row = 0; row< 9; row++){
+    strHTML += "<tr>";
+        for (let col = 0; col < 9; col++){
+            strHTML += `<td id = "R${row}C${col}"> </td>`;
+        }
+    strHTML += "</tr>";
+    }
+    strHTML += "</table>";
+    document.getElementById('solvedTable').innerHTML = strHTML;
+    
+};
+
+function calculate(){ 
+   
+    
+    
+};
+
+function saveTable(){
+    for(var row = 0; row < 9; row++){
+        sudokuArr[row] = [];
+        for(var col = 0; col < 9; col++){   
+            var currentPointer = Number(document.getElementById("r" + row + "c" + col).value);
+            if ( currentPointer <= 9 && currentPointer >= 1 ) {
+                sudokuArr[row][col] = currentPointer;
+            } else {
+                sudokuArr[row][col] = 0;
+            }
+        }
+    }
+};
