2127ce5abedbdc87da7459ca710a41458f9e0576
okmanl <ericgumba@gmail.com>
2017-07-22 18:41:32 -0700

    added test buttons

--- javascript.js
+++ javascript.js
@@ -155,6 +155,17 @@ function testSolvedTable() {
 
 }
 
+function testSolution() {
+    // for (var row = 0; row < 9; row++){
+    //     sudokuArr[row] = [];
+    // }
+
+    sudokuArr = [5, 0, 0, 0, 0, 0, 0, 2, 0], [0, 0, 0, 0, 2, 9, 0, 0, 0], [0, 0, 0, 6, 5, 8, 7, 3, 4], [3, 0, 9, 0, 7, 0, 4, 1, 0], [1, 0, 0, 0, 0, 0, 0, 0, 8], [0, 6, 8, 0, 3, 0, 2, 0, 7], [8, 5, 7, 3, 1, 6, 0, 0, 0], [0, 0, 0, 4, 9, 0, 0, 0, 0], [0, 3, 0, 0, 0, 0, 0, 0, 1];
+
+    calculate();
+
+}
+
 // SAFE
 function saveTable() {
     for (var row = 0; row < 9; row++) {
